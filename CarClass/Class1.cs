50d670738e56f9a6e10b3de47a60d2c7a8eba1b6
Leo Staf <lest0823@storsjogymnasiet.se>
2023-03-27 13:52:59 +0200

    Initial commit

---
+++ CarClass/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Car
+    {
+        public string brand;
+        public string color;
+        public int stock;
+        public int price;
+    }
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            ChooseCar();
+        }
+
+        static void ChooseCar()
+        {
+            Car car1 = new Car();
+            Console.WriteLine("Select brand: ");
+            car1.brand = Console.ReadLine();
+            Console.WriteLine("Select color: ");
+            car1.color = Console.ReadLine();
+            car1.stock = new Random().Next(1, 500);
+
+            Console.WriteLine("\nBrand: " + car1.brand);
+            Console.WriteLine("Color: " + car1.color);
+            Console.WriteLine("In stock: " + car1.stock);
+            car1.price = new Random().Next(500, 50000);
+            Console.WriteLine("Price: $" + car1.price);
+
+            Console.WriteLine("Purchase? (y/n)");
+            if (Console.ReadLine() == "y")
+            {
+                Console.WriteLine("Congratulations! You just bought a " + car1.color + " " + car1.brand + "for $" + car1.price);
+            }
+
+            Console.WriteLine("\nLook up another car? (y/n)");
+            if (Console.ReadLine() == "y")
+            {
+                Console.WriteLine("\n");
+                Main();
+            }
+        }
+    }
+}
